3334cbd64501546dc4e037c50da1f8ba7e003a12
Edward Boland <mr.eboland@gmail.com>
2020-07-14 20:11:46 -0400

    testing with firebase database with new user logins

--- src/LoginCreate.js
+++ src/LoginCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import firebase from "./firebase.js";
 
 class LoginCreate extends Component {
     constructor() {
@@ -15,6 +16,8 @@ class LoginCreate extends Component {
         // Prevent default on form submit so page doesn't refresh
         event.preventDefault();
 
+        this.newAccount();
+
         // Update state here to empty when user logs in or makes new account (latter of which will probably be it's own page)
 
         // decide where after account creation we redirect the user to.
@@ -29,12 +32,42 @@ class LoginCreate extends Component {
         })
     }
 
+    newAccount = async () => {
+        try {
+            const user = await firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
+            console.log(user.uid)
+
+            const userInfo = {
+                fName: this.state.firstName,
+                lName: this.state.lastName,
+            }
+            this.writeUserData(userInfo);
+
+        } catch (error) {
+            console.log(error.message)
+        }
+        firebase.auth().onAuthStateChanged(user => {
+            if(user) {
+                console.log(user.uid) 
+                console.log(user.email)  }
+        })
+    }
+
+    checkLogIn = () => {
+    }
+
+    writeUserData = (user) => {
+        firebase.database().ref('users/' + user.uid).set(user).catch(error => {
+            console.log(error.message)
+        });
+    }
+
     render() {
         return(
             <div>
                 <h1>Create new user here!</h1>
 
-                <form action="submit">
+                <form action="submit" onSubmit={this.onSubmit}>
                     <fieldset>
                         <label htmlFor="firstName">First Name</label>
                         <input type="text" value={this.state.name} onChange={this.onChange} name="firstName" />
